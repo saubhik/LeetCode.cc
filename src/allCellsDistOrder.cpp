48ea5fab3c9f5824fc9e018d81985216fb55c72d
saubhik <saubhik.mukherjee@gmail.com>
2019-10-28 16:23:43 +0900

    solved problem 1030

---
+++ src/allCellsDistOrder.cpp
@@ -0,0 +1,36 @@
+//
+// Created by saubhik on 2019/10/28.
+//
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    // O(n log n) where n is the number of cells
+    vector<vector<int>> allCellsDistOrder(int R, int C, int r0, int c0) {
+        vector<vector<int>> cells;
+
+        for (int i = 0; i < R; i++)
+            for (int j = 0; j < C; j++) {
+                int coords[] = {i, j};
+                cells.emplace_back(coords, coords + sizeof(coords) / sizeof(int));
+            }
+
+        sort(cells.begin(), cells.end(), [&](auto &left, auto &right) {
+            return (abs(left[0] - r0) + abs(left[1] - c0)) < (abs(right[0] - r0) + abs(right[1] - c0));
+        });
+
+        return cells;
+    }
+};
+
+int main() {
+    Solution obj;
+    vector<vector<int>> grid = obj.allCellsDistOrder(1, 2, 0, 0);
+    for (const vector<int> &v : grid) {
+        for (int x : v) cout << x << ' ';
+        cout << endl;
+    }
+}
