782c2541970b8ad6d540732eb532acfa5055bb7e
saubhik <saubhik.mukherjee@gmail.com>
2019-11-23 00:58:02 +0900

    solved problem 1008

--- src/leetcode/construct-binary-search-tree-from-preorder-traversal.cpp
+++ src/leetcode/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,6 +1,9 @@
 //
 // Created by saubhik on 2019/11/22.
 //
+#include "../utils/btree/bTreeUtils.h"
+#include <stack>
+#include <tuple>
 #include <vector>
 using namespace std;
 
@@ -13,34 +16,48 @@ struct TreeNode {
 
 class Solution {
 public:
+  // 83.42% run-time, 100% memory
+  // O(n) time, O(h) space.
+  // Hint: think in terms of l & r bounds whenever there is BST.
   static TreeNode *bstFromPreorder(vector<int> &preorder) {
-    vector<TreeNode *> ancestors;
-
-    auto sentinel = new TreeNode(INT_MAX);
-    sentinel->left = new TreeNode(preorder[0]);
-
-    ancestors.push_back(sentinel);
-    ancestors.push_back(sentinel->left);
-
+    stack<tuple<TreeNode *, int, int>> st;
+    auto root = new TreeNode(preorder[0]);
+    st.emplace(root, INT_MIN, INT_MAX);
     int i = 1, n = preorder.size();
     for (; i < n;) {
-      auto grandParent = *(ancestors.rbegin() - 1);
-      auto parent = *ancestors.rbegin();
+      auto tup = st.top();
 
-      if (preorder[i] < grandParent->val) {
-        if (preorder[i] < parent->val) {
-          parent->left = new TreeNode(preorder[i++]);
-          ancestors.push_back(parent->left);
+      auto node = get<0>(tup);
+      auto l = get<1>(tup);
+      auto r = get<2>(tup);
+
+      if (l < preorder[i] && preorder[i] < r) {
+        if (preorder[i] < node->val) {
+          node->left = new TreeNode(preorder[i++]);
+          st.emplace(node->left, l, node->val);
         } else {
-          parent->right = new TreeNode(preorder[i++]);
-          ancestors.push_back(parent->right);
+          node->right = new TreeNode(preorder[i++]);
+          st.emplace(node->right, node->val, r);
         }
       } else {
-        ancestors.pop_back();
+        st.pop();
       }
     }
+    return root;
+  }
+};
 
-    return sentinel->left;
+class Solution2 {
+public:
+  // recursive, by Lee
+  int i = 0;
+  TreeNode *bstFromPreorder(vector<int> &A, int bound = INT_MAX) {
+    if (i == A.size() || A[i] > bound)
+      return NULL;
+    TreeNode *root = new TreeNode(A[i++]);
+    root->left = bstFromPreorder(A, root->val);
+    root->right = bstFromPreorder(A, bound);
+    return root;
   }
 };
 
@@ -48,4 +65,5 @@ int main() {
   TreeNode *ans;
   vector<int> preOrder = {8, 5, 1, 7, 10, 12};
   ans = Solution::bstFromPreorder(preOrder);
+  showBTree((BinTree<int>::Node *)(ans));
 }
