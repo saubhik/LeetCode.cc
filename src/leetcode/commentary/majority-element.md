4f4a355d2280a60952b5b3689d0ff7b46bfa08b0
saubhik <saubhik.mukherjee@gmail.com>
2019-12-08 23:19:55 +0900

    solved problem 169

--- src/leetcode/commentary/majority-element.md
+++ src/leetcode/commentary/majority-element.md
@@ -11,3 +11,33 @@ Next, hash map. My solution. Keep the counts in a hash map, `unordered_map` in C
 Next, sorting. If we sort, the majority element will come in the ${(n/2)}^{th}$ place. Must be $O(nlogn)$ time, $O(1)$ space algorithm.
 
 Next, divide and conquer. This takes $O(nlogn)$ time, and $O(logn)$ space due to the recursion stack.
+
+Next, `Boyer-Moore Voting Algorithm`. We are looking for a suffix $suf$ of `nums` where $suf[0]$ is the majority element in that suffix. We maintain a count, which is incremented whenever we see an instance of our current candidate for majority element and decremented whenever we see anything else. Whenever `count` equals 0, we effectively forget about everything in `nums` up to the current index and consider the current number as the candidate for majority element.
+
+Example: [7, 7, 5, 7, 5, 1 | 5, 7 | 5, 5, 7, 7 | 7, 7, 7, 7].
+
+This is like some voodoo shit. We think the first one as the candidate whenever our count becomes 0. I think I got the hang of it. Upto some extent. This is a kind of recursively solving the problem, and throwing out prefixes of the array that we know does not help any more in finding a solution to the problem. Not to mention, this is a a first-class algorithm.
+
+Next, and finally, we have the `bit manipulation` solution. Courtesy: *jianchao-li*.
+
+```cpp
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int majority = 0;
+        for (unsigned int i = 0, mask = 1; i < 32; i++, mask <<= 1) {
+            int bits = 0;
+            for (int num : nums) {
+                if (num & mask) {
+                    bits++;
+                }
+            }
+            if (bits > nums.size() / 2) {
+                majority |= mask;
+            }
+        }
+        return majority;
+    }
+};
+```
+
--- src/leetcode/majority-element.cpp
+++ src/leetcode/majority-element.cpp
@@ -70,16 +70,67 @@ public:
   }
 };
 
+class Solution5 {
+public:
+  // Moore Voting Algorithm
+  // O(n) time, O(1) space.
+  // add +1 whenever we find candidate, and -1 whenever we find something else.
+  // set element as candidate, whenever our count becomes 0.
+  // it is as if we are starting afresh, forgetting our previous experience.
+  // in our previous experience (the prefix that we throw away), we ensure that
+  // there is no majority element.
+  // this is like recursively solving a problem.
+  // finally we will have a situation where we are running with a candidate
+  // and our count does not end up with 0. that is our solution.
+  // this is pure madness.
+  static int majorityElement(vector<int> &nums) {
+    int count = 0, candidate = 0;
+    for (int num : nums) {
+      if (!count)
+        candidate = num;
+      count += num == candidate ? 1 : -1;
+    }
+    return candidate;
+  }
+};
+
+class Solution6 {
+public:
+  // 46.22% run-time, 96.97% memory.
+  // Courtesy: jianchao-li
+  // Bit Manipulation
+  // the bits in the majority are just the majority bits of all the numbers.
+  // we traverse all the numbers 32 times, and check which bits are set, by
+  // checking if a bit is set for more than half of the numbers. If a bit is set
+  // for more than half the size of the array, that bit must belong to the
+  // majority.
+  // O(n) time, O(1) space.
+  static int majorityElement(vector<int> &nums) {
+    int bitCount, ans = 0;
+    for (unsigned int i = 0, mask = 1; i < 32; ++i, mask <<= 1) {
+      bitCount = 0;
+      for (int num : nums)
+        if (num & mask)
+          ++bitCount;
+      if (bitCount > nums.size() / 2)
+        ans |= mask;
+    }
+    return ans;
+  }
+};
+
 int main() {
   vector<int> nums;
 
   nums = {3, 2, 3};
-  printf("%d %d %d %d\n", Solution::majorityElement(nums),
+  printf("%d %d %d %d %d %d\n", Solution::majorityElement(nums),
          Solution2::majorityElement(nums), Solution3::majorityElement(nums),
-         Solution4::majorityElement(nums));
+         Solution4::majorityElement(nums), Solution5::majorityElement(nums),
+         Solution6::majorityElement(nums));
 
   nums = {2, 2, 1, 1, 1, 2, 2};
-  printf("%d %d %d %d", Solution::majorityElement(nums),
+  printf("%d %d %d %d %d %d", Solution::majorityElement(nums),
          Solution2::majorityElement(nums), Solution3::majorityElement(nums),
-         Solution4::majorityElement(nums));
+         Solution4::majorityElement(nums), Solution5::majorityElement(nums),
+         Solution6::majorityElement(nums));
 };
