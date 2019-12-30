e63440a3e750af2504db4571dc78d57b4341a3a0
saubhik <saubhik.mukherjee@gmail.com>
2019-12-30 12:14:46 +0530

    aliter 268

--- src/leetcode/missing-number.cpp
+++ src/leetcode/missing-number.cpp
@@ -18,10 +18,24 @@ public:
   }
 };
 
+class Solution2 {
+public:
+  // 24ms, 82.45% run-time; 9.8MB, 94.12% memory.
+  // using Gauss' formula, O(n) time, O(1) space.
+  static int missingNumber(vector<int> &nums) {
+    int n = nums.size(), s = 0;
+    for (int num : nums)
+      s += num;
+    return n * (n + 1) / 2 - s;
+  }
+};
+
 int main() {
   vector<int> nums = {3, 0, 1};
-  printf("%d\n", Solution::missingNumber(nums));
+  printf("%d %d\n", Solution::missingNumber(nums),
+         Solution2::missingNumber(nums));
 
   nums = {9, 6, 4, 2, 3, 5, 7, 0, 1};
-  printf("%d", Solution::missingNumber(nums));
+  printf("%d %d", Solution::missingNumber(nums),
+         Solution2::missingNumber(nums));
 }
