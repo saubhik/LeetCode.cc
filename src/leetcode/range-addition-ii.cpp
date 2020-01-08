003e5387c7dbc1b1061729b71e9b7114a0a936a4
saubhik <saubhik.mukherjee@gmail.com>
2020-01-08 20:41:20 +0700

    fixed 598

--- src/leetcode/range-addition-ii.cpp
+++ src/leetcode/range-addition-ii.cpp
@@ -6,8 +6,14 @@ using namespace std;
 
 class Solution {
 public:
+  // Runtime: 4 ms, faster than 99.65% of C++ online submissions for Range
+  // Addition II. Memory Usage: 11.4 MB, less than 100.00% of C++ online
+  // submissions for Range Addition II.
+  //
   // O(|ops|) time, O(1) space
   static int maxCount(int m, int n, vector<vector<int>> &ops) {
+    if (ops.empty())
+      return m * n;
     int minA = 40000, minB = 40000;
     for (auto &op : ops)
       minA = min(minA, op[0]), minB = min(minB, op[1]);
