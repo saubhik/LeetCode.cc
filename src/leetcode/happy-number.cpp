de2dce79449da6ec627104cd0fb30e9e5fcab740
saubhik <saubhik.mukherjee@gmail.com>
2020-02-10 23:42:32 +0900

    problem 202

--- src/leetcode/happy-number.cpp
+++ src/leetcode/happy-number.cpp
@@ -30,12 +30,37 @@ public:
 };
 
 class Solution2 {
+private:
+  static int sumSquareDigits(int n) {
+    int m = 0;
+    while (n) {
+      m += (n % 10) * (n % 10);
+      n /= 10;
+    }
+    return m;
+  }
+
 public:
-  // TODO: use Floyd's Tortoise & Hare Cycle Detection technique
-  bool isHappy(int n) {}
+  // Runtime: 0 ms, faster than 100.00% of C++ online submissions for Happy
+  // Number. Memory Usage: 8.2 MB, less than 100.00% of C++ online submissions
+  // for Happy Number.
+  //
+  // O(1) space using Floyd's Cycle Detection Algorithm.
+  static bool isHappy(int n) {
+    int slow = n, fast = n;
+    do {
+      slow = sumSquareDigits(slow);
+      fast = sumSquareDigits(sumSquareDigits(fast));
+      if (fast == 1)
+        return true;
+    } while (slow != fast);
+    return false;
+  }
 };
 
 int main() {
   printf("%d\n", Solution().isHappy(19));
+  printf("%d\n", Solution2::isHappy(19));
   printf("%d\n", Solution().isHappy(58));
+  printf("%d\n", Solution2::isHappy(58));
 }
