52ece81d251afd73521f2feea361d9ed38909bf3
saubhik <saubhik.mukherjee@gmail.com>
2020-03-06 13:10:07 +0900

    problem 338 - add fengcc's solution of dropping lowest set bit

--- src/leetcode/counting-bits.cpp
+++ src/leetcode/counting-bits.cpp
@@ -10,7 +10,7 @@ public:
   // Counting Bits.
   //
   // O(n) time, one-pass, O(n) space.
-  // Do not use any built in functions.
+  // Do not use any built-in functions.
   static std::vector<int> countBits(int num) {
     std::vector<int> ans(num + 1, 0);
     for (int curr = 1, tmp = curr; curr < num + 1; tmp = curr)
@@ -20,15 +20,34 @@ public:
   }
 };
 
+class Solution2 {
+public:
+  // Courtesy: fengcc
+  // Runtime: 48 ms, faster than 99.47% of C++ online submissions for Counting
+  // Bits. Memory Usage: 8.6 MB, less than 100.00% of C++ online submissions for
+  // Counting Bits.
+  //
+  // O(n) time, one-pass, O(n) space.
+  // Do not use any built-in functions.
+  //
+  // Idea: i&(i-1) drops the lowest set bit.
+  static std::vector<int> countBits(int num) {
+    std::vector<int> ans(num + 1, 0);
+    for (unsigned i = 1; i <= num; ++i)
+      ans[i] = ans[i & (i - 1)] + 1;
+    return ans;
+  }
+};
+
 int main() {
   std::vector<int> ans;
-  ans = Solution::countBits(2);
+  ans = Solution2::countBits(2);
   for (int elem : ans)
     printf("%d ", elem);
 
   printf("\n");
 
-  ans = Solution::countBits(5);
+  ans = Solution2::countBits(5);
   for (int elem : ans)
     printf("%d ", elem);
 }
